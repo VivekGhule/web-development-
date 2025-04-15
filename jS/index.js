915a98527b9c9c37a981e44ca2b681b8b9ec95d3
Vivek <vivekghule777@gmail.com>
2025-04-15 11:16:56 +0530

    second changes

--- jS/index.js
+++ jS/index.js
@@ -1,3 +1,11 @@
-function addNumbers(a, b) {
-    return a + b;
+// sever call - rainy , cloudy , sunny
+
+let weather = "sunny";
+
+if (weather === "sunny") {
+  console.log("It's a sunny day!");
+}else if (weather === "cloudy") {
+  console.log("It's a cloudy day!");
+}else {
+    console.log("It's a rainy day!");
 }
