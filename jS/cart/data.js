aa3749f2c64a65b01127ee1de0819a72ec138025
Vivek <vivekghule777@gmail.com>
2026-02-10 21:26:49 +0530

    Add initial HTML structure and product data for shopping cart

---
+++ jS/cart/data.js
@@ -0,0 +1,38 @@
+[
+    {
+        productId: 1,
+        productName : "Chair",
+        productPrice : "200",
+        productImg : "./Chair-PNG-HD.png"
+    },
+    {
+        productId: 2,
+        productName : "Chair",
+        productPrice : "400",
+        productImg : "./Chair-PNG-HD.png"
+    },
+    {
+        productId: 3,
+        productName : "Chair",
+        productPrice : "600",
+        productImg : "./Chair-PNG-HD.png"
+    },
+    {
+        productId: 4,
+        productName : "Chair",
+        productPrice : "800",
+        productImg : "./Chair-PNG-HD.png"
+    },
+    {
+        productId: 5,
+        productName : "Chair",
+        productPrice : "900",
+        productImg : "./Chair-PNG-HD.png"
+    },
+    {
+        productId: 6,
+        productName : "Chair",
+        productPrice : "650",
+        productImg : "./Chair-PNG-HD.png"
+    },
+]
